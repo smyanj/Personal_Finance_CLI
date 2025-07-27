adc4460e02a335aaf85872ba29bd117e94792ad4
smyanj <smyanjaip@gmail.com>
2025-07-26 22:19:15 -0700

    myStocks fully completed, has functionality to remove, add, and view current stock portfolio

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import typer
 
+from myStocks import get_stock_data, display_stock_data, add_to_watchlist, load_watchlist, remove_from_watchlist
+
+#initializing app
 app = typer.Typer()
 
 
 #Initial app: gives the stocks I'm currently investing in
 @app.command()
-def top3(stock):
-    pass
+def topStocks():
+    watchlist = load_watchlist()
+    stocks = get_stock_data(watchlist)
+    display_stock_data(stocks)
+
+@app.command()
+def add_company(ticker:str):
+    result = add_to_watchlist(ticker)
+    typer.echo(result)
+
+def remove_company(ticker:str):
+    result = remove_from_watchlist(ticker)
+    typer.echo(result)
 
 #Gives recommnedation for today
 @app.command()
